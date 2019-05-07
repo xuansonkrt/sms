4c0c1bc8016f6936d1480af19069b6649abcd423
xuansonkrt <xuansonkrt@gmail.com>
2019-05-07 19:01:18 +0700

    sonnx

---
+++ sms/DAO/NhanVienDAO.cs
@@ -0,0 +1,63 @@
+﻿using sms.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sms.DAO
+{
+    class NhanVienDAO
+    {
+        private static NhanVienDAO instance;
+
+        internal static NhanVienDAO Instance
+        {
+            get { if (instance == null) instance = new NhanVienDAO(); return instance; }
+            private set { instance = value; }
+        }
+        public List<NhanVienDTO> GetDSNV()
+        {
+            List<NhanVienDTO> list = new List<NhanVienDTO>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("dbo.USP_NHANVIEN_Getall");
+            foreach (DataRow item in data.Rows)
+            {
+                NhanVienDTO nhanVien = new NhanVienDTO(item);
+                list.Add(nhanVien);
+            }
+            return list;
+        }
+        public bool InsertNV(string hoten, DateTime ngsinh, string gioitinh, string diachi, string sdt, int luong)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery(" EXEC USP_NHANVIEN_Insert @hoten , @ngsinh , @gioitinh , @diachi , @sdt , @luong ", new object[] { hoten, ngsinh, gioitinh, diachi, sdt, luong });
+
+            return result > 0;
+        }
+        public bool UpdateNV(int maNV, string hoten, DateTime ngsinh, string gioitinh, string diachi, string sdt, int luong)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery(" EXEC USP_NHANVIEN_Update @maNV , @hoten , @ngsinh , @gioitinh , @diachi , @sdt , @luong ", new object[] { maNV, hoten, ngsinh, gioitinh, diachi, sdt, luong });
+
+            return result > 0;
+        }
+        public bool DeleteNV(int maNV)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery(" EXEC USP_NHANVIEN_Delete @maNV ", new object[] { maNV });
+
+            return result > 0;
+        }
+        public List<NhanVienDTO> SearchNV(string str)
+        {
+            List<NhanVienDTO> NhanVienList = new List<NhanVienDTO>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_NHANVIEN_Search @search ", new object[] { str });
+            foreach (DataRow item in data.Rows)
+            {
+                NhanVienDTO nhan = new NhanVienDTO(item);
+                NhanVienList.Add(nhan);
+            }
+            return NhanVienList;
+        }
+
+
+    }
+}
---
+++ sms/DTO/NhanVienDTO.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sms.DTO
+{
+    class NhanVienDTO
+    {
+        private int maNV;
+        private string hoTen;
+        private DateTime ngaySinh;
+        private string diaChi;
+        private string gioiTinh;
+        private int luong;
+        private string sDT;
+
+        //public int MaNV
+        //{
+        //    get => maNV; set => maNV = value; }
+        //public string HoTen
+        //{
+        //    get => hoTen; set => hoTen = value; }
+        //public DateTime NgaySinh
+        //{
+        //    get => ngaySinh; set => ngaySinh = value; }
+        //public string DiaChi
+        //{
+        //    get => diaChi; set => diaChi = value; }
+        //public string GioiTinh
+        //{
+        //    get => gioiTinh; set => gioiTinh = value; }
+        //public int Luong
+        //{
+        //    get => luong; set => luong = value; }
+        //public string SDT
+        //{
+        //    get => sDT; set => sDT = value; }
+
+        public NhanVienDTO()
+        {
+            // default constructor
+        }
+        public NhanVienDTO(int maNV, string hoTen, DateTime ngaySinh, string diaChi, string gioiTinh, int luong, string sDT)
+        {
+            this.maNV = maNV;
+            this.hoTen = hoTen;
+            this.ngaySinh = ngaySinh;
+            this.diaChi = diaChi;
+            this.gioiTinh = gioiTinh;
+            this.luong = luong;
+            this.sDT = sDT;
+        }
+        public NhanVienDTO(DataRow row)
+        {
+            Int32.TryParse(row["MANV"].ToString(), out this.maNV);
+            this.hoTen = row["HOTEN"].ToString();
+            this.ngaySinh = (DateTime)row["NGAYSINH"];
+            this.diaChi = row["DIACHI"].ToString();
+            this.gioiTinh = row["GIOITINH"].ToString();
+            Int32.TryParse(row["LUONG"].ToString(), out this.luong);
+            this.sDT = row["SDT"].ToString();
+        }
+    }
+}
